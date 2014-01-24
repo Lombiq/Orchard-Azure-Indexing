663b3c1956dcabeb517ef4e365d4eebb6d884722
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-01-24 21:13:42 +0100

    BlobMutexManager is now not operating with exceptions

--- AzureDirectory/BlobMutexManager.cs
+++ AzureDirectory/BlobMutexManager.cs
@@ -5,16 +5,29 @@ using System.Threading;
 
 namespace Lucene.Net.Store.Azure
 {
+    // See: https://github.com/richorama/AzureDirectory/issues/8
     public static class BlobMutexManager
     {
         public static Mutex GrabMutex(string name)
         {
             var mutexName = "luceneSegmentMutex_" + name;
-            try
+
+            Mutex mutex;
+            var notExisting = false;
+
+            if (Mutex.TryOpenExisting(mutexName, MutexRights.Synchronize | MutexRights.Modify, out mutex))
             {
-                return Mutex.OpenExisting(mutexName);
+                return mutex;
             }
-            catch (WaitHandleCannotBeOpenedException)
+
+            // Here we know the mutex either doesn't exist or we don't have the necessary permissions.
+
+            if (!Mutex.TryOpenExisting(mutexName, MutexRights.ReadPermissions | MutexRights.ChangePermissions, out mutex))
+            {
+                notExisting = true;
+            }
+
+            if (notExisting)
             {
                 var worldSid = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
                 var security = new MutexSecurity();
@@ -23,7 +36,7 @@ namespace Lucene.Net.Store.Azure
                 var mutexIsNew = false;
                 return new Mutex(false, mutexName, out mutexIsNew, security);
             }
-            catch (UnauthorizedAccessException)
+            else
             {
                 var m = Mutex.OpenExisting(mutexName, MutexRights.ReadPermissions | MutexRights.ChangePermissions);
                 var security = m.GetAccessControl();
@@ -34,7 +47,31 @@ namespace Lucene.Net.Store.Azure
 
                 return Mutex.OpenExisting(mutexName);
             }
-        }
 
+            //try
+            //{
+            //    return Mutex.OpenExisting(mutexName);
+            //}
+            //catch (WaitHandleCannotBeOpenedException)
+            //{
+            //    var worldSid = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+            //    var security = new MutexSecurity();
+            //    var rule = new MutexAccessRule(worldSid, MutexRights.FullControl, AccessControlType.Allow);
+            //    security.AddAccessRule(rule);
+            //    var mutexIsNew = false;
+            //    return new Mutex(false, mutexName, out mutexIsNew, security);
+            //}
+            //catch (UnauthorizedAccessException)
+            //{
+            //    var m = Mutex.OpenExisting(mutexName, MutexRights.ReadPermissions | MutexRights.ChangePermissions);
+            //    var security = m.GetAccessControl();
+            //    var user = Environment.UserDomainName + "\\" + Environment.UserName;
+            //    var rule = new MutexAccessRule(user, MutexRights.Synchronize | MutexRights.Modify, AccessControlType.Allow);
+            //    security.AddAccessRule(rule);
+            //    m.SetAccessControl(security);
+
+            //    return Mutex.OpenExisting(mutexName);
+            //}
+        }
     }
 }
