da2c26171ad2bf36c494683a4ba1c0b59731b864
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-02-03 17:51:19 +0100

    Documenting constants

--- Constants.cs
+++ Constants.cs
@@ -7,6 +7,9 @@ namespace Lombiq.Hosting.Azure.Indexing
 {
     public static class Constants
     {
+        /// <summary>
+        /// Configuration key for the connection string of the Azure storage account that will be used to store the Lucene indices.
+        /// </summary>
         public const string LuceneStorageStorageConnectionStringSettingName = "Lombiq.Hosting.Azure.IndexingStorageConnectionString";
     }
 }
